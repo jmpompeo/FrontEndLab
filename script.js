a653435f02ae2258e9d9b91b2023e4191d193b35
jmpompeo <joeypompeo@gmail.com>
2021-03-22 14:57:24 -0400

    lab upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let name = document.getElementById("name");
+let password = document.getElementById("password");
+let form = document.getElementById("form");
+let errorElement = document.getElementById('error');
+let email = document.getElementById("email");
+let regex = new RegExp(/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/);
+let phone = document.getElementById("number");
+let regex1 = new RegExp(/^[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$/);
+
+form.addEventListener("submit", (e) => {
+    let messages = []
+    if (name.value === '' || name.value === null) {
+        messages.push('A name is required');
+    }
+
+    if (password.value.length <= 8) {
+        messages.push('Invalid Password. Please try again');
+    }
+
+    if (regex.test(email)) {
+        messages.push('Invalid email, please try again');
+    }
+
+    if (regex.test(phone)) {
+        messages.push('Invalid phone number.');
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ');
+    }
+})
+
+
+
+
+
